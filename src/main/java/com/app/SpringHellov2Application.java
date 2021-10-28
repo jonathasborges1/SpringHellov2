53a699797deeec6f2a9df40c1829e43ba51461aa
jonathasborges1 <jbc@icomp.ufam.edu.br>
2021-10-28 13:38:25 -0400

    Hello World v2

---
+++ src/main/java/com/app/SpringHellov2Application.java
@@ -0,0 +1,19 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+
+ Referencia: https://spring.io/guides/gs/spring-boot/
+ 
+*/
+
+@SpringBootApplication
+public class SpringHellov2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHellov2Application.class, args);
+	}
+
+}
